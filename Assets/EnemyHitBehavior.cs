62e9e73ba66cd85a115a09ac51054fd4d49161d9
Mohamed Hamdy <mohamedhamdy231995@gmail.com>
2018-01-24 07:05:01 +0200

    hit man without collision

---
+++ Assets/EnemyHitBehavior.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitBehavior : MonoBehaviour {
+
+    public GameObject target;
+    public float hitDamage = 0;
+    public float speed = 1;
+    public Rigidbody2D rigid;
+    
+    
+    public bool isWork;
+    public float coolDown;
+    public float maxDis = 1.2f;
+    Animator anim;
+    public float hp = 100;
+
+    
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponentInChildren<Animator>();
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        
+        if (target)
+        {
+            
+            float dis = transform.position.x - target.transform.position.x;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= (dis / Mathf.Abs(dis));
+            transform.localScale = theScale;
+
+            float dir = target.transform.position.x - transform.position.x;
+            rigid.velocity = new Vector2(dir / Mathf.Abs(dir) * speed, 0);
+            isWork = true;
+
+
+        }
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (target)
+        {
+
+
+
+            float dir = target.transform.position.x - transform.position.x;
+            if (Mathf.Abs(dir) > maxDis)
+            {
+
+                rigid.velocity = new Vector2(dir / Mathf.Abs(dir) * speed, 0);
+                anim.SetTrigger("walk");
+                float dis = transform.position.x - target.transform.position.x;
+                Vector3 theScale = transform.localScale;
+                theScale.x = (dis / Mathf.Abs(dis));
+                transform.localScale = theScale;
+            }
+            else
+            {
+                rigid.velocity = Vector2.zero;
+                anim.SetTrigger("idle");
+                if (isWork)
+                {
+                    anim.SetTrigger("cut");
+                    //StopCoroutine(CoolDown());
+                    StartCoroutine(CoolDown());
+                }
+
+            }
+        }
+        else
+        {
+            rigid.velocity = Vector2.zero;
+            anim.SetTrigger("idle");
+        }
+
+        isDie();
+
+
+    }
+
+    void isDie()
+    {
+        if (hp <= 0)
+        {
+            anim.SetTrigger("die");
+            
+        }
+
+    }
+
+
+
+    
+
+    IEnumerator CoolDown()
+    {
+        isWork = false;
+        yield return new WaitForSeconds(coolDown);
+        isWork = true;
+    }
+
+
+}
---
+++ Assets/EnemyTrajectory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrajectory : MonoBehaviour {
+
+    
+    public GameObject target;
+    public Transform throwPoint;
+    public GameObject ball;
+    public float speed = 1;
+    public float timeTillHit = 1f;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Throw()
+    {
+        float xdistance;
+        xdistance = target.transform.position.x - throwPoint.position.x;
+
+        float ydistance;
+        ydistance = target.transform.position.y - throwPoint.position.y;
+
+        float throwAngle;
+
+        throwAngle = Mathf.Atan((ydistance + 4.905f * (timeTillHit * timeTillHit)) / xdistance);
+
+        float totalValue = xdistance / (Mathf.Cos(throwAngle) * timeTillHit);
+
+        float xValue, yValue;
+        xValue = totalValue * Mathf.Cos(throwAngle);
+        yValue = totalValue * Mathf.Sin(throwAngle);
+
+        GameObject bulletInstance = Instantiate(ball, throwPoint.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+        Rigidbody2D rigid;
+        rigid = bulletInstance.GetComponent<Rigidbody2D>();
+        //ball.GetComponent<ShurikenCollision>().hitdamage = GetComponent<MonkeyState>().hitdamage;
+        rigid.velocity = new Vector2(xValue * speed, yValue * speed);
+    }
+
+
+
+
+
+}
